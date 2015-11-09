225b289d6f996f82bc14d06b4a3f2fb41ace6009
Alex Rock Ancelet <pierstoval@gmail.com>
2015-11-09 10:43:24 +0100

    Upgrade init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :esteren do
   name 'Esteren plugin'
   author 'Alex "Pierstoval" Rock Ancelet'
   description 'Esteren tools for Redmine'
-  version '0.1.0'
+  version '0.2.2'
   url 'http://esteren.org'
   author_url 'http://www.Orbitale.io'
 
@@ -20,7 +20,7 @@ Redmine::Plugin.register :esteren do
   menu(
     :top_menu,
     :esteren_time_entries,
-    {:controller => :time_entries, :action => :index},
+    {:controller => :timelog, :action => :index},
     :caption => :label_time_tracking,
     :if => Proc.new {
       User.current.allowed_to?(:view_time_entries, nil, :global => true) or
